166a16c464a6eb96d615c0cb606f4512753d6c60
Dylan-Watts4 <dylanuk2001@hotmail.co.uk>
2025-01-23 02:29:49 +0000

    Cloudflare config

--- server/config.sh
+++ server/config.sh
@@ -10,4 +10,8 @@ sudo chmod +x /home/jenga/server/storage.sh
 sudo mv /home/jenga/server/storage.service /etc/systemd/system/storage.service
 sudo systemctl enable --now storage.service
 
+sudo useradd -r -s /bin/false cloudflared
+sudo mkdir -p /home/cloudflared/.cloudflared
+sudo chown -R cloudflared:cloudflared /home/cloudflared/.cloudflared
+
 echo "Done!"

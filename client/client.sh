08fa97485b2115931ec50561111503e81530849e
Dylan-Watts4 <dylanuk2001@hotmail.co.uk>
2025-01-24 12:43:16 +0000

    Added a counted restart for the gadget

--- client/client.sh
+++ client/client.sh
@@ -1,10 +1,16 @@
-sleep_time=5m
-
-# Calculate the max_iterations before restart
-sleep_minutes=$(echo $sleep_time | sed 's/m//')
+counter=0
 
 while :
 do
-    sleep $sleep_time
+    sleep 10m
     /usr/bin/python /home/jenga/client/main.py
+    counter=$((counter + 1))
+    echo "Counter: $counter"
+    if [ $counter -eq 18 ]; then
+        echo "Restarting the gadget..."
+        sudo modprobe -r g_mass_storage
+        sleep 10s
+        sudo modprove g_mass_storage file=/usb-drive.img stall=0 ro=0 removable=1
+        counter=0
+    fi
 done
